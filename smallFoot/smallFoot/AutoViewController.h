00c2cd88dbf06d9791b5774cffc032135f27a794
Noah Klugman <nklugman@umich.edu>
2013-04-01 15:41:55 -0400

    Added the AutoMAGIC view controller and removed 'MVP' labels

---
+++ smallFoot/smallFoot/AutoViewController.h
@@ -0,0 +1,13 @@
+//
+//  AutoViewController.h
+//  smallFoot
+//
+//  Created by Noah Klugman on 4/1/13.
+//  Copyright (c) 2013 Klugman, Tsai & Perkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AutoViewController : UIViewController
+
+@end
---
+++ smallFoot/smallFoot/AutoViewController.m
@@ -0,0 +1,38 @@
+//
+//  AutoViewController.m
+//  smallFoot
+//
+//  Created by Noah Klugman on 4/1/13.
+//  Copyright (c) 2013 Klugman, Tsai & Perkins. All rights reserved.
+//
+
+#import "AutoViewController.h"
+
+@interface AutoViewController ()
+
+@end
+
+@implementation AutoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
