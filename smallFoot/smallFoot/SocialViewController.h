174bed996049fa22e0a02e7c0952c2f3ebee12bb
Winnie Tsai <winniet@umich.edu>
2013-04-01 15:17:32 -0400

    added social view controller

---
+++ smallFoot/smallFoot/SocialViewController.h
@@ -0,0 +1,13 @@
+//
+//  SocialViewController.h
+//  smallFoot
+//
+//  Created by w on 4/1/13.
+//  Copyright (c) 2013 Klugman, Tsai & Perkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SocialViewController : UIViewController
+
+@end
---
+++ smallFoot/smallFoot/SocialViewController.m
@@ -0,0 +1,38 @@
+//
+//  SocialViewController.m
+//  smallFoot
+//
+//  Created by w on 4/1/13.
+//  Copyright (c) 2013 Klugman, Tsai & Perkins. All rights reserved.
+//
+
+#import "SocialViewController.h"
+
+@interface SocialViewController ()
+
+@end
+
+@implementation SocialViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
